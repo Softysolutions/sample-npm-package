595f42ae91f814e5f22342a2a4dc10924b079cdf
Rahul <rdhole95@gmail.com>
2024-05-25 12:54:29 +0000

    sample webpack project

---
+++ examples/webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public'),
+        },
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+        ],
+    },
+};
